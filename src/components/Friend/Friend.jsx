e117040ca99a7cc1a7eeab179f1eb6352b926ab9
maksim stryuts <maxim.stryuts@gmail.com>
2022-10-09 21:04:37 +0300

    .

--- src/components/Friend/Friend.jsx
+++ src/components/Friend/Friend.jsx
@@ -3,17 +3,19 @@ import css from '../Friend/Friend.module.css';
 import { SiStatuspage } from 'react-icons/si';
 
 export const Friend = ({ avatar, name, isOnline }) => (
-  <li className={css.item}>
-    <span className={css.status}>
-      {isOnline ? (
-        <SiStatuspage style={{ color: 'green' }} />
-      ) : (
-        <SiStatuspage style={{ color: 'red' }} />
-      )}
-    </span>
-    <img className={css.avatar} src={avatar} alt="User avatar" width="58" />
-    <p className={css.name}>{name}</p>
-  </li>
+  <>
+    <li className={css.item}>
+      <span className={css.status}>
+        {isOnline ? (
+          <SiStatuspage style={{ color: 'green' }} />
+        ) : (
+          <SiStatuspage style={{ color: 'red' }} />
+        )}
+      </span>
+      <img className={css.avatar} src={avatar} alt="User avatar" width="58" />
+      <p className={css.name}>{name}</p>
+    </li>
+  </>
 );
 
 Friend.propTypes = {
--- src/components/Friendlist/Friendlist.jsx
+++ src/components/Friendlist/Friendlist.jsx
@@ -3,16 +3,18 @@ import css from '../Friendlist/Friendlist.module.css';
 import { Friend } from 'components/Friend/Friend';
 
 export const Friendlist = ({ friends }) => (
-  <ul className={css.friendList}>
-    {friends.map(friend => (
-      <Friend
-        key={friend.id}
-        avatar={friend.avatar}
-        isOnline={friend.isOnline}
-        name={friend.name}
-      />
-    ))}
-  </ul>
+  <>
+    <ul className={css.friendList}>
+      {friends.map(friend => (
+        <Friend
+          key={friend.id}
+          avatar={friend.avatar}
+          isOnline={friend.isOnline}
+          name={friend.name}
+        />
+      ))}
+    </ul>
+  </>
 );
 
 Friendlist.propTypes = {
