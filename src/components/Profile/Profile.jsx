5e9f3ea16b9b8fb22f828cab325df79f26f10141
maksim stryuts <maxim.stryuts@gmail.com>
2022-10-09 23:01:20 +0300

    .

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,15 +1,19 @@
 import PropTypes from 'prop-types';
 import css from '../Profile/Profile.module.css';
+import video from '../Profile/a.mp4';
 
 export const Profile = ({ username, tag, location, avatar, stats }) => (
   <div className={css.profile}>
     <div className="description">
-      <img
+      <video width="320" height="500" autoPlay controls>
+        <source src={video} type="video/mp4" />
+      </video>
+      {/* <img
         src={avatar}
         alt="User avatar"
         className={css.avatar}
         width="320px"
-      />
+      /> */}
       <p className={css.name}>{username}</p>
       <p className={css.tag}>@{tag}</p>
       <p className={css.location}>{location}</p>
